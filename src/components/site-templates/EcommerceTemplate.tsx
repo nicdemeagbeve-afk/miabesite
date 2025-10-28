d92ede99d352b4de9e6b5ef3512579d419e419d5
AGBEVE Ayao Nicodème <nicdeme.agbeve@gmail.com>
2025-10-28 15:19:34 +0000

    [dyad] wrote 1 file(s)

--- src/components/site-templates/EcommerceTemplate.tsx
+++ src/components/site-templates/EcommerceTemplate.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { MessageSquare, ShoppingCart, Store, Phone, Mail, Facebook, Instagram, ChevronUp } from 'lucide-react';
+import { MessageSquare, ShoppingCart, Store, Phone, Mail, Facebook, Instagram, ChevronUp, Menu, X } from 'lucide-react'; // Added Menu and X
 import { cn } from '@/lib/utils';
 
 interface SiteData {
