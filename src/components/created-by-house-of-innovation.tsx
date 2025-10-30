06328d39b33a5ca61a7daaafee2393fb41f67955
AGBEVE Ayao Nicodème <nicdeme.agbeve@gmail.com>
2025-10-30 01:42:48 +0000

    [dyad] Remplacement de "Made with Dyad" par "Créer par House Of Innovation" - wrote 1 file(s), renamed 1 file(s) + extra files edited outside of Dyad

--- src/components/created-by-house-of-innovation.tsx
+++ src/components/created-by-house-of-innovation.tsx
@@ -1,13 +1,13 @@
-export const MadeWithDyad = () => {
+export const CreatedByHouseOfInnovation = () => {
   return (
     <div className="p-4 text-center">
       <a
-        href="https://www.dyad.sh/"
+        href="https://houseofinnovation.com"
         target="_blank"
         rel="noopener noreferrer"
         className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
       >
-        Made with Dyad
+        Créer par House Of Innovation
       </a>
     </div>
   );
