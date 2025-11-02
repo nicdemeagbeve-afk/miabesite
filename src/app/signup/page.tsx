a42be42fd9385680904a3fc949b89172abe036bb
AGBEVE Ayao Nicodème <nicdeme.agbeve@gmail.com>
2025-11-02 00:59:48 +0000

    hhhmmm

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
-import SignupForm from "@/components/auth/SignupForm"; // Corrected import
+import { SignupForm } from "@/components/auth/SignupForm"; // Changement ici : import nommé
 
 export default function SignupPage() {
     return (
-      <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Chargement...</div>}>
-        <SignupForm />
-      </Suspense>
+        <Suspense fallback={<div className="flex items-center justify-center min-h-screen">Chargement...</div>}>
+            <SignupForm />
+        </Suspense>
     );
 }
